969dbf520733e35b54edafa363b09a3ba2b0f827
Nguyen Gia Huy <134088678+HZeroxium@users.noreply.github.com>
2024-07-26 16:24:08 +0700

    schema: add copyright for Footer

--- src/payload/globals/Footer.ts
+++ src/payload/globals/Footer.ts
@@ -9,6 +9,12 @@ export const Footer: GlobalConfig = {
   },
   fields: [
     {
+      name: 'copyright',
+      label: 'Copyright',
+      type: 'text',
+      required: false,
+    },
+    {
       name: 'navItems',
       type: 'array',
       maxRows: 6,
